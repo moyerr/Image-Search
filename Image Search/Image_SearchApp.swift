1e37a2c389877c2bfbb8ab5bb3f30de0a628eec7
Robbie Moyer <rmoyer08@gmail.com>
2023-02-16 16:14:08 -0500

    Initial Commit

---
+++ Image Search/Image_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_SearchApp.swift
+//  Image Search
+//
+//  Created by Robert Moyer on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Image_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
